aac43e13fae4f347b356053ab3f848271767bee3
iremhatipoglu <62469438+iremhatipoglu@users.noreply.github.com>
2022-01-24 16:40:40 +0300

    Add files via upload
    Operatör alıştırmalarının olduğu patika  c# pratiği

---
+++ csharpPatika2/csharpPatika2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace csharpPatika2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Atama ve İşlemli atama Operatörleri");
+
+            //Atama ve İşlemli Atama
+            int x = 3;
+            int y = 3;
+            y = y + 2;
+
+            Console.WriteLine(y);
+            y += 2;
+            Console.WriteLine(y);
+            y/= 1;
+            Console.WriteLine(y);
+            x *= 2;
+            Console.WriteLine(x);
+
+            //Mantıksal Operatörler
+            // ||, &&, !
+
+            bool isSuccess = true;
+            bool isCompleted = false;
+
+            if (isSuccess && isCompleted)
+                Console.WriteLine("Perfect");
+            if (isSuccess || isCompleted)
+                Console.WriteLine("Great");
+            if (isSuccess && !isCompleted)
+                Console.WriteLine("Fine");
+
+            //İlişkisel Operatörler
+            // <,>,<=,>=,==,!=
+
+            int a = 3;
+            int b = 4;
+            bool sonuc = a < b; 
+
+            Console.WriteLine(sonuc);
+            sonuc = a > b;
+            Console.WriteLine(sonuc);
+            sonuc = a >= b;
+            Console.WriteLine(sonuc);
+            sonuc = a <= b;
+            Console.WriteLine(sonuc);
+            sonuc = a == b;
+            Console.WriteLine(sonuc);
+            sonuc = a != b;
+            Console.WriteLine(sonuc);
+
+            //Aritmetik Operatörler
+            // +,*,/,-,%
+
+        }
+    }
+}
